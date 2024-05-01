6e537698976db63715edeeedb5b2a5ea1cdad4a4
NoahCardoza <noahcardoza@gmail.com>
2024-04-30 20:11:53 -0700

    update firmware.py

--- firmware.py
+++ firmware.py
@@ -1,4 +1,4 @@
-from time import sleep
+import gc
 
 import dht
 import utime
@@ -8,26 +8,30 @@ uart = UART(0, 38400, tx=Pin(0), rx=Pin(1))
 temp_sensor = dht.DHT22(Pin(2))
 trigger = Pin(21, Pin.OUT)
 echo = Pin(20, Pin.IN)
+led = Pin(25, Pin.OUT)
+soil = ADC(Pin(26))
 
 
 def get_soil_moisture():
-    soil = ADC(Pin(26))
     min_moisture = 20000
     max_moisture = 65535
-    moisture = (max_moisture - soil.read_u16()) * 100 / (max_moisture - min_moisture)
-    # print("moisture: " + "%.2f" % moisture +"% (adc: "+str(soil.read_u16())+")")
-    return moisture
+    moisture_percent = (
+        (max_moisture - soil.read_u16()) * 100 / (max_moisture - min_moisture)
+    )
+    return moisture_percent
 
 
 def get_temp_and_hum():
-    temp_sensor.measure()
+    try:
+        temp_sensor.measure()
+    except Exception:  # should be Exception("checksum error")
+        led.toggle()
+        utime.sleep(0.1)
+        get_temp_and_hum()
+        return (1000, 1000)
     temp = temp_sensor.temperature()
     hum = temp_sensor.humidity()
     temp_f = temp * (9 / 5) + 32.0
-
-    # print('Temperature: %3.1f C' %temp)
-    # print('Temperature: %3.1f F' %temp_f)
-    # print('Humidity: %3.1f %%' %hum)
     return (temp_f, hum)
 
 
@@ -42,10 +46,9 @@ def get_distance():
     while echo.value() == 1:
         signalon = utime.ticks_us()
 
-    timepassed = signalon - signaloff
-    distance = (timepassed * 0.0343) / 2
-    # print(distance, "cm")
-    return distance
+    time_passed = signalon - signaloff
+    distance_cm = (time_passed * 0.0343) / 2  # cm
+    return distance_cm
 
 
 def get_payload():
@@ -55,19 +58,16 @@ def get_payload():
     )
 
 
-# print("start")
-# for i in range(10):
-#   t = "ping," + str(i)
-#   uart.write(bytes(t, "ascii"))
-#    print(t)
-#    sleep(1)
-
-#
-while True:
-    # get_temp_and_hum()
-    # get_soil_moisture()
-    # get_distance()
-    p = get_payload()
-    print(p)
-    uart.write(bytes(p, "ascii"))
-    utime.sleep(5)
+try:
+    while True:
+        utime.sleep(5)
+        led.on()
+        p = get_payload()
+        uart.write(bytes(p, "ascii"))
+        led.off()
+        gc.collect()
+except Exception as e:
+    uart.write(bytes(repr(e), "ascii"))
+    while True:
+        led.toggle()
+        utime.sleep(1)
